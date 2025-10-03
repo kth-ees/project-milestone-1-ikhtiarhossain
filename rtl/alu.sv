bbe535164002d12809155873224a9c4e66e63b7c
Ikhtiar Hossain <120584860+ikhtiarhossain@users.noreply.github.com>
2025-10-03 13:06:35 +0200

    task 1

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,17 +1,39 @@
 module alu #(
-  BW = 16 // bitwidth
+  parameter int BW = 16 // bitwidth
   ) (
-  input  logic unsigned [BW-1:0] in_a,
-  input  logic unsigned [BW-1:0] in_b,
-  input  logic             [3:0] opcode,
-  output logic unsigned [BW-1:0] out,
-  output logic             [2:0] flags // {overflow, negative, zero}
+  input  logic signed [BW-1:0] in_a,
+  input  logic signed [BW-1:0] in_b,
+  input  logic        [2:0] opcode,
+  output logic signed [BW-1:0] out,
+  output logic        [2:0] flags // {overflow, negative, zero}
   );
 
-  // Complete your RTL code here
+  always_comb begin
+    out = '0;
+    case (opcode)
+      3'b000: out   = in_a + in_b;
+      3'b001: out   = in_a - in_b;
+      3'b010: out   = in_a & in_b;
+      3'b011: out   = in_a | in_b;
+      3'b100: out   = in_a ^ in_b;
+      3'b101: out   = in_a + 1'b1;
+      3'b110: out   = in_a;
+      3'b111: out   = in_b;
+      default: out  = '0;
+    endcase
+  end
+
+  always_comb begin
+    flags[2] = 1'b0;
+    case (opcode)
+      3'b000: flags[2]  = ~(in_a[BW-1] ^ in_b[BW-1]) & (in_a[BW-1] ^ out[BW-1]);
+      3'b001: flags[2]  = (in_a[BW-1] ^ in_b[BW-1]) & (in_a[BW-1] ^ out[BW-1]);
+      3'b101: flags[2]  = ~in_a[BW-1] & out[BW-1];
+      default: flags[2] = 1'b0;
+    endcase
+  end
+
+  assign flags[1] = out[BW-1];
+  assign flags[0] = ~|out;
 
 endmodule
-
-
-
-
