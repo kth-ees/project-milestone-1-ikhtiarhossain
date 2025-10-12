70b42e80737a4c002de023c2b75e9a9a50100a86
Ikhtiar Hossain <120584860+ikhtiarhossain@users.noreply.github.com>
2025-10-12 16:11:21 +0200

    cleaner task1

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -10,30 +10,36 @@ module alu #(
 
   always_comb begin
     out = '0;
+
     case (opcode)
-      3'b000: out   = in_a + in_b;
-      3'b001: out   = in_a - in_b;
-      3'b010: out   = in_a & in_b;
-      3'b011: out   = in_a | in_b;
-      3'b100: out   = in_a ^ in_b;
-      3'b101: out   = in_a + 1'b1;
-      3'b110: out   = in_a;
-      3'b111: out   = in_b;
-      default: out  = '0;
+      3'b000: out = in_a + in_b;          // ADD
+      3'b001: out = in_a - in_b;          // SUB
+      3'b010: out = in_a & in_b;          // AND
+      3'b011: out = in_a | in_b;          // OR
+      3'b100: out = in_a ^ in_b;          // XOR
+      3'b101: out = in_a + 1'b1;          // INC
+      3'b110: out = in_a;                 // MOVA
+      3'b111: out = in_b;                 // MOVB
+      default: out = '0;
     endcase
   end
 
   always_comb begin
-    flags[2] = 1'b0;
-    case (opcode)
-      3'b000: flags[2]  = ~(in_a[BW-1] ^ in_b[BW-1]) & (in_a[BW-1] ^ out[BW-1]);
-      3'b001: flags[2]  = (in_a[BW-1] ^ in_b[BW-1]) & (in_a[BW-1] ^ out[BW-1]);
-      3'b101: flags[2]  = ~in_a[BW-1] & out[BW-1];
-      default: flags[2] = 1'b0;
-    endcase
-  end
+    logic ovf;
+    ovf = 1'b0;
 
-  assign flags[1] = out[BW-1];
-  assign flags[0] = ~|out;
+    case (opcode)
+      3'b000: // ADD ovf
+        ovf = ~(in_a[BW-1] ^ in_b[BW-1]) & (in_a[BW-1] ^ out[BW-1]);
+      3'b001: // SUB ovf
+        ovf = (in_a[BW-1] ^ in_b[BW-1]) & (in_a[BW-1] ^ out[BW-1]);
+      3'b101: // INC ovf
+        ovf = ~in_a[BW-1] & out[BW-1];
+      default: 
+        ovf = 1'b0;
+    endcase
+
+    flags = {ovf, out[BW-1], ~|out}; // {overflow, negative, zero}
+  end
 
 endmodule
